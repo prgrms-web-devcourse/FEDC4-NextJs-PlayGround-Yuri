eaa850dec7d57fac38c1cdece7f83c68584bce06
kutta97 <yhjin04170@gmail.com>
2023-10-05 11:44:27 +0900

    Routing: Dynamic Routes - Dynamic Segments: 동적 데이터를 통해 경로를 생성하려는 경우 사용 - params: Typescript 사용 시, route segment 에 따라 params 에 타입 추가

--- 양혜진/my-app/app/(marketing)/blog/page.tsx
+++ 양혜진/my-app/app/(marketing)/blog/page.tsx
@@ -1,3 +1,28 @@
+import PostList, { Post } from "@/app/(marketing)/blog/PostList";
+
+const posts: Post[] = [
+  {
+    id: '1',
+    title: 'post01',
+    slug: 'post01-slug'
+  },
+  {
+    id: '2',
+    title: 'post02',
+    slug: 'post02-slug'
+  },
+  {
+    id: '3',
+    title: 'post03',
+    slug: 'post03-slug'
+  }
+]
+
 export default function Page() {
-  return <h1>Hello, Blog Page!</h1>
+  return (
+    <div>
+      <h1>Hello, Blog Page!</h1>
+      <PostList posts={posts} />
+    </div>
+  )
 }
